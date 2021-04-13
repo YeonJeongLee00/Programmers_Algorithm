6362d4ee223697e5471cba410f13c9f482342b70
yeonjeonglee <yeonjeong.lee00@gmail.com>
2021-04-13 12:02:08 +0900

    feat - 전화번호 목록 문제 추가, 해결

--- src/listOfPhoneNumbers.java
+++ src/listOfPhoneNumbers.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
+import java.util.*;
+/*
+* 프로그래머스 - 전화번호 목록
+* */
 public class listOfPhoneNumbers {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+
+        Arrays.sort(phone_book);
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if (phone_book[i + 1].startsWith(phone_book[i])) {
+                answer = false;
+                break;
+            }
+
+        }
+
+        return answer;
+    }
+
 }
