4cf5483351ec12efd2dc30ad6db12230de12cc43
yeonjeonglee <yeonjeong.lee00@gmail.com>
2021-04-13 11:40:36 +0900

    feat - 위장 문제 추가, 문제 해결

--- src/Camouflage.java
+++ src/Camouflage.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
+/*
+* 프로그래머스 - 위장
+* */
+class Camouglage {
+    public int Solution(String[][] clothes) {
+        int answer = 1;
 
-public class Camouflage {
+        HashMap<String, Integer> hs = new HashMap<String, Integer>();
+
+        for (int i = 0; i < clothes.length; i++) {
+            int num = 1;
+            if(hs.containsKey(clothes[i][1])){
+                num = hs.get(clothes[i][1]) + 1;
+            }
+            hs.put(clothes[i][1], num);
+        }
+
+        Object[] val = hs.values().toArray();
+        for (int i = 0; i <val.length ; i++) {
+            answer = answer * ((int)val[i] + 1);
+        }
+
+        return answer - 1;
+    }
 }
